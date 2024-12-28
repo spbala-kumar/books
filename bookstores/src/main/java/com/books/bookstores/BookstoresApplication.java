ebe29ea77d7167d7d39fb092e0667b2910e3755d
balakumar <spbala_kumar@yahoo.com>
2024-12-28 23:04:34 +0530

    Initial commit with Spring Boot book store project

---
+++ bookstores/src/main/java/com/books/bookstores/BookstoresApplication.java
@@ -0,0 +1,13 @@
+package com.books.bookstores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoresApplication.class, args);
+	}
+
+}
---
+++ bookstores/src/test/java/com/books/bookstores/BookstoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.bookstores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
